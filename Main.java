e72051efeb2ec7aba5c3f882bc16e6e1db9c34f1
rahmd0012 <rahmd0012@wrdsb.ca>
2020-10-01 14:01:57 +0000

    Finding the max integer out of 3 numbers

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * program finds the max integer out of 3 integers
+ * @author Daniel Rahmani
  */
 public class Main {
 
@@ -9,7 +11,31 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // Create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // get the first integer from the user
+    System.out.println("What is your first integer?");
+    int firstInt = input.nextInt();
+
+    // get the second integer from the user
+    System.out.println("What is your second integer?");
+    int secondInt = input.nextInt();
+
+    // get the third integer from the user
+    System.out.println("What is your third integer?");
+    int thirdInt = input.nextInt();
+
+    // check which number is the biggest
+    if(firstInt > secondInt && firstInt > thirdInt) {
+      System.out.println(firstInt + " is the max number");
+    } else if(secondInt > firstInt && secondInt > thirdInt) {
+      System.out.println(secondInt + " is the max number");
+    }else {
+      System.out.println(thirdInt + " is the max number");
+    }
+    }
+
     
-  }
+  
 }
